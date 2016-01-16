d94458768acfddbb6718731021541f424fc969d0
kachorroski <kernelkill@outlook.com>
2016-01-16 00:36:33 -0300

    Projeto workshop Algaworks

---
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -0,0 +1,36 @@
+package com.algaworks.cobranca.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.repository.Titulos;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	@Autowired
+	private Titulos titulos;
+	
+	@RequestMapping("/novo")
+	public String novo(){
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST )
+	public String salvar(Titulo titulo){
+		//TODO: Salvar no banco de dados. 
+		
+//		System.out.println(">>>" + titulo.getDescricao());
+//		System.out.println(">>>" +titulo.getDataVencimento());
+//		System.out.println(">>>" + titulo.getValor());
+//		System.out.println(">>>" + titulo.getStatus());
+//		
+		titulos.save(titulo);
+		return "CadastroTitulo"; 
+	}
+
+}
