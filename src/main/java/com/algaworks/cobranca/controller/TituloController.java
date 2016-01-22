d6937ee70aeaef24159cdb5badb1da63b4cba6ac
Joabe Kachorroski <kernelkill@outlook.com>
2016-01-22 01:55:13 -0300

    Atualizando git no Fedora

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 
@@ -17,24 +22,26 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo(){
-		return "CadastroTitulo";
+	public ModelAndView novo(){
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST )
 	public ModelAndView salvar(Titulo titulo){
 		//TODO: Salvar no banco de dados. 
-		
-//		System.out.println(">>>" + titulo.getDescricao());
-//		System.out.println(">>>" +titulo.getDataVencimento());
-//		System.out.println(">>>" + titulo.getValor());
-//		System.out.println(">>>" + titulo.getStatus());
-//		
-		titulos.save(titulo);
+	
+		titulos.save(titulo); 
 		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Titulo Salvo com sucesso!");
 		return mv; 
+	}
+	
+	
+	@ModelAttribute
+	public List<StatusTitulo> todosStatusTitulos(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }
